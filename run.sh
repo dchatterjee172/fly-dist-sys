30357f7f478425bbc720e31f6e643831084573eb
dchatterjee172 <dchatterjee172@gmail.com>
2023-03-20 09:44:01 +0530

    add a script to run all tests

---
+++ run.sh
@@ -0,0 +1,17 @@
+set -eux
+
+go build -o bin/echo -C echo
+(cd maelstrom && ./maelstrom test -w echo --bin ../echo/bin/echo --node-count 1 --time-limit 10)
+
+
+go build -o bin/uniqueid -C uniqueid
+(cd maelstrom && ./maelstrom test -w unique-ids --bin ../uniqueid/bin/uniqueid --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition)
+
+go build -o bin/broadcast -C broadcast
+(cd maelstrom && ./maelstrom test -w broadcast --bin ../broadcast/bin/broadcast --node-count 5 --time-limit 20 --rate 10 --nemesis partition)
+
+go build -o bin/gocounter -C gocounter
+(cd maelstrom && ./maelstrom test -w g-counter --bin ../gocounter/bin/gocounter --node-count 3 --rate 100 --time-limit 20 --nemesis partition)
+
+go build -o bin/kafka -C kafka
+(cd maelstrom && ./maelstrom test -w kafka --bin ../kafka/bin/kafka --node-count 2 --concurrency 2n --time-limit 20 --rate 1000)
